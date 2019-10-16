7d6401d626b0de372eddd8c85bf15fd6cd2f7d09
AbrTatyana <gapaagapa@yandex.ru>
2019-10-16 23:23:52 +0300

    First task

--- tasks/prolog/1_simple_predicates.pl
+++ tasks/prolog/1_simple_predicates.pl
@@ -11,7 +11,46 @@
 	father(b,e).  % 4
 	father(c,f).  % 5
 % указать в каком порядке и какие ответы генерируются вашими методами
-	?- brother(X,Y).
-	?- cousin(X,Y).
-	?- grandson(X,Y).
-	?- descendent(X,Y).
+
+	brother(X,Y):- father(Z,X), father(Z,Y), X \= Y.
+	cousin(X,Y):- brother(Z,W), father(Z,X), father(W,Y), X \= Y.
+	grandson(X,Y):- father(Y,Z), father(Z,X).
+	descendent(X,Y):- father(Y,X).
+	descendent(X,Y):- father(Y,Z), descendent(X,Z).
+
+:- writeln('List of brothers:').
+:- forall(brother(X,Y), (write(X), write(' and '), writeln(Y))).
+
+:- writeln('List of cousins:').
+:- forall(cousin(X,Y), (write(X), write(' and '), writeln(Y))).
+
+:- writeln('List of grandsons:').
+:- forall(grandson(X,Y), (write(X), write(' is grandson '), writeln(Y))).
+
+:- writeln('List descendents:').
+:- forall(descendent(X,Y), (write(X), write(' is descendent '), writeln(Y))).
+
+ 
+% List of brothers:
+% b and c
+% c and b
+% d and e
+% e and d
+% List of cousins:
+% d and f
+% e and f
+% f and d
+% f and e
+% List of grandsons:
+% d is grandson a
+% e  is grandson a
+% f is grandson a
+% List descendents:
+% b is descendent a
+% c is descendent a
+% d is descendent b
+% e is descendent b
+% f is descendent c
+% d is descendent a
+% e is descendent a
+% f is descendent a
