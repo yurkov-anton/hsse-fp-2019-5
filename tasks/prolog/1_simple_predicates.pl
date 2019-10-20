e1c0027416df2f0e524f88133aed65d9973718b0
Dmitriy <gdk1743@gmail.com>
2019-10-20 21:27:50 +0300

    done prolog/1_simple_predicate

--- tasks/prolog/1_simple_predicates.pl
+++ tasks/prolog/1_simple_predicates.pl
@@ -1,17 +1,26 @@
-% есть набор фактов вида father(person1, person2) (person1 is the father of person2)
-% Необходимо определить набор предикатов:
-% 1. brother(X,Y)    -  определяющий являются ли аргументы братьями
-% 2. cousin(X,Y)     -  определяющий являются ли аргументы двоюродными братьями
-% 3. grandson(X,Y)   -  определяющий является ли аргумент Х внуком аргумента Y
-% 4. descendent(X,Y) -  определяющий является ли аргумент X потомком аргумента Y
-% 5. используя в качестве исходных данных следующий граф отношений
-	father(a,b).  % 1                 
-	father(a,c).  % 2
-	father(b,d).  % 3
-	father(b,e).  % 4
-	father(c,f).  % 5
-% указать в каком порядке и какие ответы генерируются вашими методами
-	?- brother(X,Y).
-	?- cousin(X,Y).
-	?- grandson(X,Y).
-	?- descendent(X,Y).
+father(a,b).
+father(a,c).
+father(b,d).
+father(b,e).
+father(c,f).
+
+brother(X,Y) :- father(Z,X), father(Z,Y), X\=Y.
+cousin(X,Y) :- father(S,X), father(W,Y), S\=W, brother(S, W).
+grandson(X,Y) :- father(Q,X), father(Y,Q).
+descendent(X,Y) :- (father(Y,X); father(Q,X), father(Y,Q)).
+
+% ?- setof([X,Y],brother(X,Y),RESULT).
+% RESULT = [[b, c], [c, b], [d, e], [e, d]]
+%
+%
+% ?- setof([X,Y],cousin(X,Y),RESULT).
+% RESULT = [[d, f], [e, f], [f, d], [f, e]]
+%
+%
+% ?- setof([X,Y],grandson(X,Y),RESULT).
+% RESULT = [[d, a], [e, a], [f, a]]
+%
+%
+% ?- setof([X,Y],descendent(X,Y),RESULT).
+% RESULT = [[b, a], [c, a], [d, a], [d, b], [e, a], [e, b], [f, a]]
+
