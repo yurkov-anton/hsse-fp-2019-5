095d803698bf192a9e7bb1d2b0f6d4604362ef93
AbrTatyana <gapaagapa@yandex.ru>
2019-10-27 16:28:26 +0300

    Second task

--- tasks/prolog/2_truth_table.pl
+++ tasks/prolog/2_truth_table.pl
@@ -10,3 +10,25 @@
 % true fail true
 % fail true fail
 % fail fail fail
+
+bool(true).
+bool(fail).
+
+and(A, B):- A, B.
+or(A, B):- A; B.
+not(A):- A = false.
+equ(A, B):- A == B.
+xor(A, B):- not(equ(A, B)).
+
+exec(E, true):- E, !.
+exec(_, fail).
+
+truth_table(A, B, E):- bool(A), bool(B), write(A), write(' '), write(B), write(' '), exec(E, Result), writeln(Result), fail.
+truth_table(_,_,_):- nl, true.
+
+?-truth_table(A,B,and(A,or(A,B))).
+
+% true true true
+% true fail true
+% fail true fail
+% fail fail fail
