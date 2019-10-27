1b64df346d73d337be575089d2149e0ab8b234fc
Daniil Sudzhaev <sudzhaev.da@gmail.com>
2019-10-27 19:59:23 +0300

    prolog tasks 1 and 2

--- tasks/prolog/2_truth_table.pl
+++ tasks/prolog/2_truth_table.pl
@@ -10,3 +10,34 @@
 % true fail true
 % fail true fail
 % fail fail fail
+
+bool(true).
+bool(fail).
+
+and(A, B) :- A, B.
+
+or(A, B) :- A; B.
+
+xor(A, B) :- A \= B.
+
+not(A) :- A = fail.
+
+equ(A,B) :- A = B.
+
+calculate(E, true) :- E, !.
+calculate(_, fail).
+
+truth_table(A, B, E) :-
+    bool(A),
+    bool(B),
+    calculate(E, R),
+    format('~w ~w ~w~n', [A, B, R]),
+    fail.
+
+?- truth_table(A, B, and(A, or(A, B))).
+/*
+true true true
+true fail true
+fail true fail
+fail fail fail
+*/
