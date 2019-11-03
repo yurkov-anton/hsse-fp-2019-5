40a356ba80f43bc7bdf599a2a1bb35ec95439f02
Pismerova Victoria <pvikka@mail.ru>
2019-11-03 10:17:45 +0300

    Pismerova prolog 2_truth_table and 3_quicksort

--- tasks/prolog/2_truth_table.pl
+++ tasks/prolog/2_truth_table.pl
@@ -10,3 +10,25 @@
 % true fail true
 % fail true fail
 % fail fail fail
+
+and(A, B) :- A, B.
+or(A, B) :- A; B.
+xor(A, B) :- or(A, B), A\= B.
+not(A) :- fail.
+equ(A, B) :- A = B.
+
+evaluate(E, true) :- E, !.
+evaluate(_, false).
+
+bool(true).
+bool(false).
+
+truth_table(A,B,E) :-
+  bool(A),
+  bool(B),
+  write(A),
+  write(' \t '),
+  write(B),
+  write(' \t '),
+  evaluate(E, Result),
+  write(Result),nl, fail.
