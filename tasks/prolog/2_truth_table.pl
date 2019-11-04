c1bc696eabf8d15c8d5bca588e01f88886c09215
think68 <56322487+think68@users.noreply.github.com>
2019-11-05 00:32:22 +0800

    the second prolog task
    the second prolog task (truth_table)

--- tasks/prolog/2_truth_table.pl
+++ tasks/prolog/2_truth_table.pl
@@ -1,4 +1,4 @@
-% определить предикаты:
+/*% определить предикаты:
 	% and(A,B)
 	% or(A, B)
 	% xor(A, B)
@@ -9,4 +9,33 @@
 % true true true
 % true fail true
 % fail true fail
-% fail fail fail
+% fail fail fail*/
+
+bool(true).
+bool(fail).
+
+and(A,B) :- A,B.
+or(A,B) :- A;B.
+xor(A,B) :- A\=B.
+not(A) :- \+ A.
+equ(A,B) :- A=B.
+
+/* Expression judge */
+eval(C,true) :- C, !.
+eval(_,fail).
+
+truth_table(A,B,C) :- 
+	bool(A),
+	bool(B),
+	write(A),
+	write('    '),
+	write(B),
+	write('    '),
+	eval(C,D),
+	writeln(D), (1=2).
+	
+%?- truth_table(A,B,and(A,or(A,B))).
+%true    true    true
+%true    fail    true
+%fail    true    fail
+%fail    fail    fail
