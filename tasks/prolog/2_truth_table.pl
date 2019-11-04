ee9428c73093e7be7262903fe0035cab21b9b02a
Daniil Shevtsov <54152044+daniil-shevtsov@users.noreply.github.com>
2019-11-04 15:23:36 +0300

    Prolog 2 daniil shevtsov (#12)
    * add first version of truth_table predicate
    
    * try to print truth table
    
    * make truth-table work but operation variable names are generated
    
    * make truth table work
    
    * clear unused code
    
    * implement the rest of the logical functions

--- tasks/prolog/2_truth_table.pl
+++ tasks/prolog/2_truth_table.pl
@@ -1,3 +1,4 @@
+% Условие
 % определить предикаты:
 	% and(A,B)
 	% or(A, B)
@@ -9,4 +10,54 @@
 % true true true
 % true fail true
 % fail true fail
-% fail fail fail
+% fail fail fai
+
+%
+% Задание
+%
+my_not(A) :- \+ A.
+my_and(A,B) :- A,B.
+my_or(A,B) :- A;B.
+my_eq(A,B) :- A=B.
+my_xor(A,B) :- A \= B.
+
+%
+% Вспомогательные предикаты
+%
+
+bool_value(true).
+bool_value(fail).
+
+% Получить стоку с 'true' или 'fail' в зависимости от значения A
+print_bool(S, A) :- A, swritef(S, true).
+print_bool(S, A) :- not(A), swritef(S, fail).
+
+% Вывести значения из списка в заданном формате
+print_values_row(Columns) :- writef("%10l%10l%w\n", Columns).
+
+% Получить комбинации X и Y
+permutations(X, Y) :- bagof(A, bool_value(A), A_values),
+		bagof(B, bool_value(B), B_values),
+		member(X,A_values),
+		member(Y,B_values).
+
+
+truth_table(A, Expression) :- truth_table(A,A,Expression).
+
+% Выводит таблицу истинности заданной операции,
+truth_table(A,B, Expression) 
+	:- writef("%10l%10l", ["A", "B"]),
+	write_term(Expression,[variable_names(['A'=A, 'B'=B])]),
+	writef("\n"),
+	permutations(A,B),
+	print_bool(SA, A), print_bool(SB, B), print_bool(SE, Expression),
+	print_values_row([SA,SB,SE]), 
+	false.
+
+% Например
+% ?- truth_table(A,B,my_and(A,my_xor(A,B))).
+% A         B         my_and(A,my_xor(A,B))
+% true      true      fail
+% true      fail      true
+% fail      true      fail
+% fail      fail      fail
