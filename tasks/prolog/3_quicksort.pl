ded8eceeb0c4da3bf56fcee93d1f2e2dbadfa1b0
Daniil Shevtsov <54152044+daniil-shevtsov@users.noreply.github.com>
2019-11-04 19:05:41 +0300

    Prolog task 3 Quick Sort (#13)
    * Prolog 2 daniil shevtsov (#12) (#2)
    
    * add first version of truth_table predicate
    
    * try to print truth table
    
    * make truth-table work but operation variable names are generated
    
    * make truth table work
    
    * clear unused code
    
    * implement the rest of the logical functions
    
    * Add prolog task 3 quick sort implementation

--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,1 +1,17 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+
+% Сначала получаем в Left - элементы из Tail меньше X, в Right - больше
+% рекурсивно получаем отсортированную левую часть - и отсортированную правую
+% результирующий список - это отсортированная левая часть, затем X, затем отсортированная правая
+qsort([],[]).   
+
+qsort([X|Tail], SortedResult) :-         
+	qsplit(X, Tail, Left, Right),
+	qsort(Left, SortedLeft),
+	qsort(Right, SortedRight),
+	append(SortedLeft, [X|SortedRight], SortedResult).
+
+% qsplit(X,Y,U,W) - Элементы из Y, которые меньше X, идут в список U, остальные - в W
+qsplit(X, [],[],[]).
+qsplit(X, [Y|Tail], [Y|Left], Right) :- X > Y, qsplit(X, Tail, Left, Right).
+qsplit(X, [Y|Tail], Left, [Y|Right]) :- qsplit(X, Tail, Left, Right).
