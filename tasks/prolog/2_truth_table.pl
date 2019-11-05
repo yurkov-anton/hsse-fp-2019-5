4c876cb6d300e48d3edfe3092f228c51e91c8e0a
unknown <mirror40@mail.ru>
2019-11-05 21:19:38 +0300

    2,3,5

--- tasks/prolog/2_truth_table.pl
+++ tasks/prolog/2_truth_table.pl
@@ -7,6 +7,27 @@
 % ипользовать предикат truth_table(A,B, expression) для построения таблиц истинности, например:
 % truth_table(A,B,and(A,or(A,B))).
 % true true true
-% true fail true
-% fail true fail
-% fail fail fail
+% true false true
+% false true false
+% false false false
+
+and(A,B) :- A, B.
+or(A,B) :- A; B.
+not(A):- A = false.
+equ(A,B) :- A = B.
+xor(A,B) :- not(equ(A,B)).
+
+trueOrFalse(X) :- X=true.
+trueOrFalse(X) :- X=false.
+
+exec(Expression) :- call(Expression), writeln("true"), !.
+exec(_) :- writeln("false"), !.
+
+truth_table(A,B,Expression) :- trueOrFalse(A), trueOrFalse(B), write(A), write(" "), write(B), write(" "), exec(Expression), false.
+truth_table(_,_,_) :- true.
+
+%truth_table(A,B,and(A,or(A,B))).
+%true true true
+%true false true
+%false true false
+%false false false
--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,1 +1,18 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+qsort([],[]).
+qsort([H|T],K) :-
+  partition(H,T,Left,Right),
+  qsort(Left,Ls),
+  qsort(Right,Rs),
+  append(Ls,[H|Rs],K).
+
+partition(_,[],[],[]).
+partition(Pivot,[H|T],[H|Left],Right) :-
+  H =< Pivot,
+  partition(Pivot,T,Left,Right).
+partition(Pivot,[H|T],Left,[H|Right]) :-
+  H > Pivot,
+  partition(Pivot,T,Left,Right).
+
+append([],L,L).
+append([H|T],L2,[H|L3]) :- append(T,L2,L3).
