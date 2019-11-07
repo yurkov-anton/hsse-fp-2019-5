2db6b640138e0246f8bef5fc9eafadc4db45917a
Denis <d-eisenach@yandex.ru>
2019-11-07 11:44:58 +0300

    Done lab_1, lab_3

--- tasks/prolog/1_simple_predicates.pl
+++ tasks/prolog/1_simple_predicates.pl
@@ -5,13 +5,41 @@
 % 3. grandson(X,Y)   -  определяющий является ли аргумент Х внуком аргумента Y
 % 4. descendent(X,Y) -  определяющий является ли аргумент X потомком аргумента Y
 % 5. используя в качестве исходных данных следующий граф отношений
-	father(a,b).  % 1                 
-	father(a,c).  % 2
-	father(b,d).  % 3
-	father(b,e).  % 4
-	father(c,f).  % 5
+%	father(a,b).  % 1                 
+%	father(b,d).  % 3
+%	father(b,e).  % 4
+%	father(c,f).  % 5
 % указать в каком порядке и какие ответы генерируются вашими методами
-	?- brother(X,Y).
-	?- cousin(X,Y).
-	?- grandson(X,Y).
-	?- descendent(X,Y).
+%	?- brother(X,Y).
+%	?- cousin(X,Y).
+%	?- grandson(X,Y).
+%	?- descendent(X,Y).
+	
+
+father(a,b).  % 1                 
+father(a,c).  % 2
+father(b,d).  % 3
+father(b,e).  % 4
+father(c,f).  % 5
+
+brother(X,Y) :- father(N,X), father(N,Y), X\=Y.
+cousin(X,Y) :- father(W,X), father(Z,Y), brother(W,Z).
+grandson(X,Y):- father(Y,Q), father(Q,X).
+descendent(X,Y) :- father(Y,X).
+descendent(X,Y) :- father(X,M), descendent(X,M).
+
+
+% ?- bagof([X,Y],brother(X,Y),RESULT).
+% RESULT = [[b, c], [c, b], [d, e], [e, d]]
+%
+%
+% ?- bagof([X,Y],cousin(X,Y),RESULT).
+% RESULT = [[d, f], [e, f], [f, d], [f, e]]
+%
+%
+% ?- bagof([X,Y],grandson(X,Y),RESULT).
+% RESULT = [[d, a], [e, a], [f, a]]
+%
+%
+% ?- bagof([X,Y],descendent(X,Y),RESULT).
+% RESULT = [[b, a], [c, a], [d, a], [d, b], [e, a], [e, b], [f, a]]
--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,1 +1,13 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+ 
+qsort([],[]).
+qsort([Elem], [Elem]).
+qsort([Pivot|Tail], Result):-
+    partition(Pivot, Tail, Smallers, Largers),
+    qsort(Smallers, SortedSmalls),
+    qsort(Largers, SortedLarges),
+    append(SortedSmalls, [Pivot|SortedLarges],Result).
+
+partition(_,[],[],[]).
+partition(Pivot, [Head|Tail], [Head|Smallers], Largers):- Head =< Pivot, partition(Pivot,  Tail, Smallers, Largers).
+partition(Pivot, [Head|Tail], Smallers, [Head|Largers]) :- Head >= Pivot, partition(Pivot, Tail, Smallers, Largers).
