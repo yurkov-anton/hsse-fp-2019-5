285def4d2dc3972b5a5e98535b42fded2de87129
Dmitriy <gdk1743@gmail.com>
2019-11-10 14:10:44 +0300

    create lab4

--- tasks/prolog/4_tree.pl
+++ tasks/prolog/4_tree.pl
@@ -1,5 +1,36 @@
-% используя предикат qsort(L,K) из предыдущего задания разработать предикат:
-% balanced_tree(L,T) - который по заданном списку строит сбалансированное бинарное дерево поиска
-% для построения дерева использовать следующие  нотации:
-% empty - пустое дерево 
-% instant(R, L, R) - бинарное дерево с корнем R и двумя поддеревьями L и R соотвественно (левое и правое)
+divide([], _Pivot, [], []):-!.
+divide([Head|Tail], Pivot, [Head|GreaterList], SmallerList):-
+  Head >= Pivot, !, divide(Tail, Pivot, GreaterList, SmallerList).
+divide([Head|Tail], Pivot, GreaterList, [Head|SmallerList]):-
+  divide(Tail, Pivot, GreaterList, SmallerList).
+
+qsort([], []).
+qsort([Elem], [Elem]).
+qsort([Pivot|Tail], SortedList):-
+  divide(Tail, Pivot, GreaterList, SmallerList),
+  qsort(GreaterList, SortedGreaterList),
+  qsort(SmallerList, SortedSmallerList),!,
+  append(SortedSmallerList, [Pivot|SortedGreaterList], SortedList).
+
+split_list(List, Left, Right):-
+    append(Left, Right, List),
+    length(List, Length),
+    Path is Length div 2,
+    length(Left, Path).
+
+balanced_tree_implementation([], empty).
+
+balanced_tree_implementation(List, instant(Root, Left, Right)):-
+    split_list(List, LeftList, [Root|RightList]),
+    balanced_tree_implementation(LeftList, Left),
+    balanced_tree_implementation(RightList, Right).
+
+balanced_tree(L,T):-
+	qsort(L,K),
+	balanced_tree_implementation(K,T).
+
+% ?- balanced_tree([3,4,2,6,5,9,8], T).
+% T = instant(5, instant(3, instant(2, empty, empty), instant(4, % empty, empty)), instant(8, instant(6, empty, empty), instant(9, % empty, empty))) .
+
+% ?- balanced_tree([], T).
+% T = empty
