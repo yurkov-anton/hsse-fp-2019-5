e0680fcd6a0c50663a0c9e6fa8ba16efd5bb1d56
merttsenn <merttsenn@gmail.com>
2019-11-12 01:42:48 +0300

    3th and 5th tasks are solved

--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,1 +1,19 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+
+split([], _Pivot, [], []):-!.
+split([Head|Tail], Pivot, [Head|Greater], Smaller):-
+  Head >= Pivot, !, split(Tail, Pivot, Greater, Smaller).
+split([Head|Tail], Pivot, Greater, [Head|Smaller]):-
+  split(Tail, Pivot, Greater, Smaller).
+
+qsort([], []).
+qsort([Pivot|Tail], SortedList):-
+  split(Tail, Pivot, Greater, Smaller),
+  qsort(Greater, SortedGreater),
+  qsort(Smaller, SortedSmaller),!,
+  append(SortedSmaller, [Pivot|SortedGreater], SortedList).
+
+/*
+?- qsort([1,5,9,4,6,0,3],X).
+X = [0, 1, 3, 4, 5, 6, 9].
+*/
--- tasks/prolog/5_merge.pl
+++ tasks/prolog/5_merge.pl
@@ -1,2 +1,18 @@
-$ определить предикат mrg(L1, L2, R) который для двух отсортированных списков L1 и L2 
+% определить предикат merge(L1, L2, R) который для двух отсортированных списков L1 и L2 
 % определяет список R, составленный из этих элементов
+
+merge([], L2, L2).
+merge(L1, [], L1).
+merge([],[],[]).
+
+merge([Head1|Tail1], [Head2|Tail2], [Head1|TailR]) :- 
+	merge(Tail1, [Head2|Tail2], TailR), Head1 => Head2.
+merge([Head1|Tail1], [Head2|Tail2], [Head2|TailR]) :- 
+	merge([Head1|Tail1], Tail2, TailR), Head2 =< Head1.
+
+
+/*
+?- merge([1,2,5],[0,1,2],X).
+X = [0, 1, 1, 2, 2, 5] .
+*/
+
