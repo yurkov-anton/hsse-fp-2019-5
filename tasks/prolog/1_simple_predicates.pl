c26d05968a131af4d970a62b43b75f676bc2e5a6
qzsss <56436411+qzsss@users.noreply.github.com>
2019-11-11 14:06:27 +0300

    first task prolog
    first task prolog

--- tasks/prolog/1_simple_predicates.pl
+++ tasks/prolog/1_simple_predicates.pl
@@ -11,7 +11,67 @@
 	father(b,e).  % 4
 	father(c,f).  % 5
 % указать в каком порядке и какие ответы генерируются вашими методами
-	?- brother(X,Y).
+/*	?- brother(X,Y).
 	?- cousin(X,Y).
 	?- grandson(X,Y).
 	?- descendent(X,Y).
+*/
+
+brother(X,Y):- father(Z,X), father(Z,Y), X \= Y.
+:- brother(X,Y).
+/*
+X = b,
+Y = c ;
+X = c,
+Y = b ;
+X = d,
+Y = e ;
+X = e,
+Y = d ;
+*/
+
+cousin(X,Y):- brother(Z,W), father(Z,X), father(W,Y), X \= Y.
+:- cousin(X,Y).
+/*
+X = d,
+Y = f ;
+X = e,
+Y = f ;
+X = f,
+Y = d ;
+X = f,
+Y = e ;
+*/
+
+grandson(X,Y):- father(Y,Z), father(Z,X).
+:- grandson(X,Y).
+/*
+X = d,
+Y = a ;
+X = e,
+Y = a ;
+X = f,
+Y = a ;
+*/
+
+descendent(X,Y):- father(Y,X).
+descendent(X,Y):- father(Y,Z), descendent(X,Z).
+:- descendent(X,Y).
+/*
+X = b,
+Y = a ;
+X = c,
+Y = a ;
+X = d,
+Y = b ;
+X = e,
+Y = b ;
+X = f,
+Y = c ;
+X = d,
+Y = a ;
+X = e,
+Y = a ;
+X = f,
+Y = a ;
+*/
