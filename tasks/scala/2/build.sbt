98667b18cfaa744c439f13dd2c0a855ae6fc0f9c
egor <egorkozelskiJ@gmail.com>
2019-11-12 16:48:23 +0300

    scala_2

--- tasks/scala/2/build.sbt
+++ tasks/scala/2/build.sbt
@@ -5,8 +5,7 @@ scalaVersion := "2.12.7"
 scalacOptions ++= Seq(
   "-deprecation",
   "-unchecked",
-  "-optimise",
-  "-Yinline-warnings"
+  "-optimise"
 )
 
 fork := true
--- tasks/scala/2/src/main/scala/funsets/Main.scala
+++ tasks/scala/2/src/main/scala/funsets/Main.scala
@@ -2,5 +2,12 @@ package funsets
 
 object Main extends App {
   import FunSets._
+  //var s1: Set = union(singletonSet(1), union(singletonSet(2), singletonSet(4)))
+  //var s2: Set = union(singletonSet(4), singletonSet(5))
+  
+  //printSet(s1)
+  //printSet(s2)
+  //printSet(intersect(s1,s2))
+  //printSet(diff(s1,s2))
   println(contains(singletonSet(1), 1))
 }
