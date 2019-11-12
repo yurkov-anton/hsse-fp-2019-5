e13495f16b2aa0188e0536f4bb86f398e7461a23
egor <egorkozelskiJ@gmail.com>
2019-11-12 13:33:10 +0300

    lab_5

--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -11,13 +11,13 @@ pivot(Pivot, [H|T], [H|LTE], GT) :-
 pivot(Pivot, [H|T], LTE, [H|GT]) :- 
 	pivot(Pivot, T, LTE, GT).
 
-quicksort([], []).
+qsort([], []).
 
-quicksort([H|T], R) :- 
+qsort([H|T], R) :- 
 	pivot(H, T, L1, L2), 
-	quicksort(L1, SL1), 
-	quicksort(L2, SL2),
+	qsort(L1, SL1), 
+	qsort(L2, SL2),
 	append(SL1, [H|SL2], R).
 
-?- quicksort([2, 3, 9, 6, 1, 8, 5, 4, 7, 10], X), writeln(X).
+?- qsort([2, 3, 9, 6, 1, 8, 5, 4, 7, 10], X), writeln(X).
 % [1,2,3,4,5,6,7,8,9,10]
