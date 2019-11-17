0ede2768867a8fcaf033b8207aa970ef33369138
Kirill Turchinskii <turchinskii.kirill@gmail.com>
2019-11-17 22:04:25 +0300

    Lab3 done

--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,1 +1,8 @@
-% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+qsort(L,K):-q_sort(L,[],K).
+q_sort([],Acc,Acc).
+q_sort([H|T],Acc,Sorted):-
+	change(H,T,L1,L2),
+	q_sort(L1,Acc,Sorted1),q_sort(L2,[H|Sorted1],Sorted).
+change(_,[],[],[]).
+change(H,[X|T],[X|L],G) :- X=<H,change(H,T,L,G).
+change(H,[X|T],L,[X|G]) :- X>H,change(H,T,L,G).
