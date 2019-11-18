b59d73a81ddc5f0f1515a881056e8c43e16902de
qzsss <56436411+qzsss@users.noreply.github.com>
2019-11-18 11:13:35 +0300

    sec task prolog

--- tasks/prolog/2_truth_table.pl
+++ tasks/prolog/2_truth_table.pl
@@ -10,3 +10,32 @@
 % true fail true
 % fail true fail
 % fail fail fail
+
+b(true).
+b(fail).
+
+and(A,B) :- A,B.
+or(A,B) :- A;B.
+xor(A, B):- not(equ(A, B))
+not(A) :- \+ A.
+equ(A,B) :- A=B.
+
+evaluate(M, true) :- M, !.
+evaluate(_, fail).
+
+truth_table(A,B,M) :-
+  b(A),
+  b(B),
+  write(A),
+  write('  '),
+  write(B),
+  write('  '),
+  evaluate(M, N),
+  writeln(N), fail.
+
+
+?-truth_table(A,B,and(A,or(A,B))).
+% true    true    true
+% true    fail    true
+% fail    true    fail
+% fail    fail    fail
