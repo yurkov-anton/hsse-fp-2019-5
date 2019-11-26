2a19af4018d2e6036670f6289d6c642c754ca97d
Sergey Lappo <sergeylappo@gmail.com>
2019-11-26 23:00:56 +0100

    labs 1 and 2 implemented

--- tasks/prolog/2_truth_table.pl
+++ tasks/prolog/2_truth_table.pl
@@ -10,3 +10,29 @@
 % true fail true
 % fail true fail
 % fail fail fail
+
+and(A,B) :- A, B.
+
+or(A,B) :- A -> true; B.
+
+not(A) :- A \= true.
+
+xor(A,B) :- or(A,B), not(and(A,B)).
+
+equ(A,B) :- A = B.
+
+bool(true).
+bool(fail).
+
+
+truth_table(A,B,Expr) :- bool(A), bool(B), do(A,B,Expr), fail.
+do(A,B,_) :- write(A), write('\t'), write(B), write('\t'), fail.
+do(_,_,Expr) :- Expr, !, write(true), nl.
+do(_,_,_) :- write(fail), nl.
+
+% ?- truth_table(A,B,and(A,or(B,B))).
+% true    true    true
+% true    fail    fail
+% fail    true    fail
+% fail    fail    fail
+% false.
