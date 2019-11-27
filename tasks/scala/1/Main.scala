c6856d0907c182fcbd225d1c800b3da43a308011
lo24 <56306376+lo24@users.noreply.github.com>
2019-11-27 12:43:33 -0800

    lab6

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -15,14 +15,28 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if (c == 0 || c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    def f(chars: List[Char], numOpens: Int): Boolean = {
+      if (chars.isEmpty) {
+        numOpens == 0
+      } else {
+        val h = chars.head
+        val n =
+          if (h == '(') numOpens + 1
+          else if (h == ')') numOpens - 1
+          else numOpens
+        if (n >= 0) f(chars.tail, n)
+        else false
+      }
+    }
+    f(chars, 0)
   }
 
   /**
@@ -32,7 +46,11 @@ object Main {
    * there is 1 way to give change for 5 if you have coins with denomiation
    * 2 and 3: 2+3.
    */
-  def countChange(money: Int, coins: List[Int]): Int = {
-
+  def countChange(money: Int, coins: List[Int]): Int = (money , coins) match {
+    case (_, x :: _) if (x == 0) => throw new IllegalArgumentException("Coin can not be equal 0");
+    case (_, Nil) => 0
+    case (tmp, _) if tmp < 0 => 0
+    case (0, _) => 1
+    case (tmp, _) => countChange(tmp, coins.tail) + countChange(tmp - coins.head, coins)
   }
 }
