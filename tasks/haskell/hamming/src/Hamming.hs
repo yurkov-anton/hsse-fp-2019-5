d734a41899559f27d27219db69b4f82a351c30d5
Aleksey Ivanov <aleksey.ivanov@arcadia.spb.ru>
2019-11-28 18:01:57 +0300

    complete Haskell task

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,11 @@
 module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
-distance xs ys = error "Implementation is lost..."
+
+distance = distance' 0 where
+
+  distance' count "" "" = Just count
+  distance' _ "" _  = Nothing
+  distance' _ _  "" = Nothing
+  distance' count (x:xs)(y:ys) | x == y = distance' count xs ys
+      | otherwise = distance' (count + 1) xs ys
