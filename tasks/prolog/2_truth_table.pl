3542a1e640ce1bf4f8b807a58300f737397e8e23
Bryan Valdez <56548047+valdezb@users.noreply.github.com>
2019-11-29 01:32:18 +0300

    Update 2_truth_table.pl
    Completed prolog task #2

--- tasks/prolog/2_truth_table.pl
+++ tasks/prolog/2_truth_table.pl
@@ -10,3 +10,27 @@
 % true fail true
 % fail true fail
 % fail fail fail
+
+and(A,B) :- A, B.
+or(A,_) :- A.
+or(_,B) :- B.
+xor(A,B) :- A \= B.
+not(A) :- xor(true, A).
+equ(A,B) :- or(and(A,B), and(not(A),not(B))).
+
+% bindv(X) :- instantiate X to be true and false successively
+
+boolv(true).
+boolv(fail).
+
+truth_table(A,B,Exp) :- boolv(A), boolv(B), do(A,B,Exp), fail.
+
+do(A,B,_) :- write(A), write('  '), write(B), write('  '), fail.
+do(_,_,Expr) :- Expr, !, write(true), nl.
+do(_,_,_) :- write(fail), nl.
+
+% ?- truth_table(A,B,and(A,or(A,B))).
+% true  true  true
+% true  fail  true
+% fail  true  fail
+% fail  fail  fail
