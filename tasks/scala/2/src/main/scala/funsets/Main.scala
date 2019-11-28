1b933ee2c18f75ffa7b3115a66c92c01a26b29b3
think68 <56322487+think68@users.noreply.github.com>
2019-11-28 21:10:55 +0800

    the second scala task
    the second scala task

--- tasks/scala/2/src/main/scala/funsets/Main.scala
+++ tasks/scala/2/src/main/scala/funsets/Main.scala
@@ -3,4 +3,19 @@ package funsets
 object Main extends App {
   import FunSets._
   println(contains(singletonSet(1), 1))
+  printSet(union(singletonSet(1),singletonSet(2)))
+  printSet(intersect(union(singletonSet(1),singletonSet(2)),union(singletonSet(2),singletonSet(3))))
+  printSet(diff(union(singletonSet(1),singletonSet(2)),singletonSet(1)))
+  printSet(filter(union(singletonSet(1),singletonSet(2)),singletonSet(1)))
+
+  //output
+  /*
+  sbt:funsets> run
+  [info] running (fork) funsets.Main 
+  [info] true
+  [info] {1,2}
+  [info] {2}
+  [info] {2}
+  [info] {1}
+  */
 }
