a65de924eceec47b7ff8774659e9a532c383260e
Elena <38397960+TaElena@users.noreply.github.com>
2019-11-28 22:00:56 +0300

    4_tree

--- tasks/prolog/4_tree.pl
+++ tasks/prolog/4_tree.pl
@@ -3,3 +3,42 @@
 % для построения дерева использовать следующие  нотации:
 % empty - пустое дерево 
 % instant(R, L, R) - бинарное дерево с корнем R и двумя поддеревьями L и R соотвественно (левое и правое)
+
+qsort([],[]).
+qsort([H|T],K) :-
+  partition(H,T,Left,Right),
+  qsort(Left,Ls),
+  qsort(Right,Rs),
+  append(Ls,[H|Rs],K).
+
+partition(_,[],[],[]).
+partition(Pivot,[H|T],[H|Left],Right) :-
+  H =< Pivot,
+  partition(Pivot,T,Left,Right).
+partition(Pivot,[H|T],Left,[H|Right]) :-
+  H > Pivot,
+  partition(Pivot,T,Left,Right).
+
+append([],L,L).
+append([H|T],L2,[H|L3]) :- append(T,L2,L3).
+
+balanced_tree([], empty).
+
+balanced_tree([H|[]], instant(H, empty, empty)) :- !.
+
+balanced_tree([T|[H|[]]], instant(H, L, empty)) :-
+    H > T,
+    balanced_tree([T], L).
+
+balanced_tree([H|[T|[]]], instant(H, L, empty)) :-
+    T >= H,
+    balanced_tree([T], L).
+
+balanced_tree(L, instant(R, Lt, Rt)) :-
+    qsort(L, LS),
+    append(LL, [R|LR], LS),
+    length(LL, LnL),
+    length([R|LR], LnR),
+    (LnL =:= LnR; LnL =:= (LnR - 1)),
+    balanced_tree(LL, Lt),
+    balanced_tree(LR, Rt), !.
