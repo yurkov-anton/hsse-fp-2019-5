99db625f85fffec6c2275215061f0f10c26a9b9f
Bryan Valdez <56548047+valdezb@users.noreply.github.com>
2019-11-29 00:23:22 +0300

    Update 3_quicksort.pl
    Complete prolog task #3

--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,1 +1,11 @@
-% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный
+qsort([],[]).
+qsort([H|T], Q) :- separate(H, T, Min, Maj), qsort(Min, M1), qsort(Maj, M2), append(M1,[H|M2],Q).
+
+%separate(Pivot, List, Minor, Major)
+separate(_,[],[],[]).
+separate(X,[A|B],[A|Min],Maj) :- A =< X, separate(X, B, Min, Maj).
+separate(X,[A|B],Min,[A|Maj]) :- A > X, separate(X, B, Min, Maj).
+
+%qsort([123, -1, 0, 3, 8, 34, 89, -25, 65], K).
+%K = [-25, -1, 0, 3, 8, 34, 65, 89, 123]
