27495eaef0e3f2a26d028f380f0bc8f1eba3256a
AbrTatyana <gapaagapa@yandex.ru>
2019-11-29 23:37:06 +0300

    1 and 2 Scala

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,5 +1,4 @@
-package recfun
-import common._
+import scala.collection.mutable.ListBuffer
 
 object Main {
   def main(args: Array[String]) {
@@ -9,19 +8,37 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
+      println(balance(List('(', ')', '(', 'x', ')')))
+      println(countChange(5, List(3,2)))
   }
 
   /**
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if (c == 0 || r == 0) 1
+    else pascal(c - 1, r - 1) + pascal(c - 1, r)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
+    def f(chars: List[Char], numOpens: Int): Boolean = {
+      if (chars.isEmpty) {
+        numOpens == 0
+      } else {
+        val h = chars.head
+        val n =
+          if (h == '(') numOpens + 1
+          else if (h == ')') numOpens - 1
+          else numOpens
+        if (n >= 0) f(chars.tail, n)
+        else false
+      }
+    }
+
+    f(chars, 0)
    
   }
 
@@ -33,6 +50,30 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
+    	def f(lastMaxCoin_total_coll: List[(Int, Int)], count: Int): Int = {
+      if (lastMaxCoin_total_coll.isEmpty) {
+        count
+      } else {
+        val b = ListBuffer[(Int, Int)]()
+        var newCount = count
+        for ((lastMaxCoin, total) <- lastMaxCoin_total_coll) {
+          if (total < money) {
+            for (c <- coins) {
+              if (c >= lastMaxCoin) {
+                val e = (c, total + c)
+                b += e
+              }
+            }
+          } else if (total == money) {
+            newCount += 1
+          }
+        }
 
+        f(b.toList, newCount)
+      }
+    }
+
+    val b = coins.map { c => (c, c) }
+    f(b, 0)
   }
 }
