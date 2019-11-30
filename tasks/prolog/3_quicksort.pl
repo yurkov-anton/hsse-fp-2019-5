81e18bd836f215bf388014c93052c2a5529c895d
yurkov-anton <36742876+yurkov-anton@users.noreply.github.com>
2019-11-30 22:24:17 +0300

    add prolog lab 3

--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,1 +1,25 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+split(Middle,[Head|Tail],[Head|L1],L2) :- 
+    Head =< Middle, 
+    split(Middle,Tail,L1,L2). 
+
+split(Middle,[Head|Tail],L1,[Head|L2]) :- 
+    Head > Middle,
+    split(Middle,Tail,L1,L2).
+
+split(_,[],[],[]).
+
+quicksort([Head|Tail],R) :-
+	split(Head,[Head|Tail],[H1|L1],L2),
+    quicksort(L1,R1),
+    quicksort(L2,R2),
+	append(R1,[H1|R2],R).
+
+quicksort([],[]).
+
+%?-quicksort([4,2,3,1],A)
+%A = [1, 2, 3, 4]
+%?-quicksort([],A)
+%A = []
+%?-quicksort([1],A)
+%A = [1]
